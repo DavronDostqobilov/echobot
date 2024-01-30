02671491c4dc47e3964d3a8dacc5a9006af8b113
DavronDostqobilov <dostqobilovdavron885@gamil.com>
2024-01-30 16:10:13 +0500

    new4

--- app.py
+++ app.py
@@ -3,14 +3,14 @@ import telegram
 # Create the application instance
 TOKEN ='6244092180:AAH_10MCMX4wAESk6TTp-iboI1EMSe6FeZ8'
 bot = telegram.Bot(TOKEN)
-chat_id = '1432402481'
+# chat_id = '1432402481'
 app = Flask(__name__)
 
 @app.route('/',methods=["POST"])
 def index():
     data=request.get_json()
     print(data)
-    bot.send_message(chat_id,text="hello word")
+    bot.send_message(chat_id=data['message']['chat']['id'],text=data['message']['text'])
     return 'index page'
 
 if __name__=="__main__":
